e9a8ddde79f99dd9d335df8495e65557305f9060
allens_aldo <56383926+InvedAllens@users.noreply.github.com>
2023-04-11 15:08:23 -0600

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["index.html","./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+    colors:{
+    'alt':'#F8F4EA'
+    }
+    },
   },
   plugins: [],
 }
